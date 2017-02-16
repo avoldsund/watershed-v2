f21468cc65799f519370785d66d896021f501704
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-02-16 17:02:57 +0100

    Change to a smaller landscape

--- experiments/plot_heights_2d.py
+++ experiments/plot_heights_2d.py
@@ -6,7 +6,7 @@ Plot the heights of the landscape in two dimensions
 """
 
 file_name = '/home/anderovo/Dropbox/watershedLargeFiles/anders_hoh.tiff'
-landscape = load_data.get_landscape_tyrifjorden(file_name)
+landscape = load_data.get_smallest_test_landscape_tyrifjorden(file_name)
 
 #plot.plot_landscape_3d(landscape, ds)
 
