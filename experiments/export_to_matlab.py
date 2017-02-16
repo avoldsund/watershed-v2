119a896e01029432035b06314cc2cf28b86b2b22
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-02-16 17:02:14 +0100

    Method for exporting data to Matlab

--- experiments/export_to_matlab.py
+++ experiments/export_to_matlab.py
@@ -2,11 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from lib import load_data, util, river_analysis, plot
 import scipy.io
+from scipy.ndimage.filters import maximum_filter
 
 # Different examples to show how the velocity field will be if the vectors point in the direction of steepest descent
-# heights_one = np.array([[20, 20, 20],
-#                         [20, 15, 20],
-#                         [20, 5, 20]])
+heights_one = np.array([[20, 20, 20],
+                        [20, 15, 20],
+                        [20, 5, 20]])
 # heights_two = np.array([[20, 15, 10],
 #                         [15, 10, 5],
 #                         [10, 5, 0]])
@@ -23,40 +24,62 @@ import scipy.io
 #                         [0, 10]])
 file_name = '/home/anderovo/Dropbox/watershedLargeFiles/anders_hoh.tiff'
 landscape = load_data.get_smallest_test_landscape_tyrifjorden(file_name)
-r, c = np.shape(landscape.heights)
+# print heights
 
-ws_of_node = river_analysis.calculate_watershed_of_node(landscape, (230, 125))
-ws_of_node_2d = util.map_1d_to_2d(ws_of_node, landscape.nx)
-# river_analysis.calculate_nr_of_upslope_cells()
-acc_flow = river_analysis.calculate_accumulated_flow(landscape)
+r, c = np.shape(landscape.heights)
+# plot.plot_landscape_2d(landscape, 1)
+# acc_flow = river_analysis.calculate_accumulated_flow(landscape)
 # plot.plot_accumulated_flow(acc_flow)
 
-print ws_of_node
-# Change name to obtain different field
-y, x = np.gradient(landscape.heights, edge_order=1)
-y /= 10
-x /= 10
-# plot.plot_watersheds_2d_coords([ws_of_node], landscape, 1)
+outlet = (208, 152)
 
-x = -x
-# print 'dx: '
-# print x
-# print 'dy: '
-# print y
-#
-# print ws_of_node_2d
-# Plot vector field
-plt.figure()
+ws_of_node, traps, trap_heights, trap_indices_in_ws, steepest_spill_pairs, flow_directions, heights = river_analysis.\
+    calculate_watershed_of_node(landscape, outlet)
 
-X = np.zeros((r, c))
-Y = np.zeros((r, c))
-X[ws_of_node_2d] = x[ws_of_node_2d]
-Y[ws_of_node_2d] = y[ws_of_node_2d]
-Q = plt.quiver(X, Y, pivot='mid', color='r', units='inches')
-plt.axis([-1, c, -1, r])
-plt.gca().invert_yaxis()
+plot.plot_watersheds_2d([ws_of_node], outlet, landscape, 1)
+# Pre-processing
+boundary = util.get_domain_boundary_indices(landscape.nx, landscape.ny)
+all_trap_nodes = np.concatenate(traps)
 
-plt.show()
 
-scipy.io.savemat('gradient.mat', dict(dx=X, dy=Y))
-scipy.io.savemat('watershed.mat', dict(ws=ws_of_node_2d))
+print [util.map_1d_to_2d(np.array([steepest_spill_pairs[el][0], steepest_spill_pairs[el][1]]), landscape.ny) for el in trap_indices_in_ws]
+print [traps[i] for i in trap_indices_in_ws]
+print 'heights[201, 156]: ', heights[201, 156]
+print 'heights[202, 156]: ', heights[202, 156]
+print 'util.map_1d_to_2d(np.array([50406]), landscape.ny): ', util.map_1d_to_2d(np.array([50406]), landscape.ny)
+for i in trap_indices_in_ws:
+    s = util.map_1d_to_2d(steepest_spill_pairs[i][1], landscape.ny)
+    if heights[s] == trap_heights[i] and steepest_spill_pairs[i][1] not in boundary and steepest_spill_pairs[i][1] \
+            not in all_trap_nodes:
+        traps[i] = np.append(traps[i], steepest_spill_pairs[i][1])
+
+nr_of_cells_in_each_trap = [len(traps[i]) for i in trap_indices_in_ws]
+total_trap_cells = np.sum(nr_of_cells_in_each_trap)
+
+traps = [util.map_1d_to_2d(traps[i], landscape.nx) for i in trap_indices_in_ws]
+nrOfTraps = len(traps)
+trap_heights = [trap_heights[t] for t in trap_indices_in_ws]
+
+new_traps = np.zeros((len(traps), 2), dtype=object)
+for i in range(len(traps)):
+    new_traps[i][0] = traps[i][0]
+    new_traps[i][1] = traps[i][1]
+
+print flow_directions[200:205, 154:158]
+ws = util.map_1d_to_2d(ws_of_node, landscape.nx)
+
+zero_wrapping = np.zeros((landscape.ny, landscape.nx), dtype=int)
+zero_wrapping[1:-1, 1:-1] = flow_directions[1:-1, 1:-1]
+flow_directions = zero_wrapping
+
+scipy.io.savemat('watershed.mat', dict(watershed=ws))
+scipy.io.savemat('heights.mat', dict(heights=landscape.heights))
+scipy.io.savemat('traps.mat', dict(traps=new_traps, totalTrapCells=total_trap_cells,
+                                   nrOfTraps=nrOfTraps, nrOfCellsInEachTrap=nr_of_cells_in_each_trap,
+                                   trapHeights=trap_heights))
+scipy.io.savemat('flowDirections.mat', dict(flowDirections=flow_directions))
+
+
+print flow_directions[200:205, 154:158]
+print heights[200:205, 154:158]
+
