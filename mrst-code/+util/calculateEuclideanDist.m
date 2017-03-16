e4f43ea472813a4738c793eadf152f967b5a1865
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-03-16 14:23:06 +0100

    Fix help text and clean up

--- mrst-code/+util/calculateEuclideanDist.m
+++ mrst-code/+util/calculateEuclideanDist.m
@@ -1,6 +1,10 @@
 function distance = calculateEuclideanDist(coords, coord)
-%CALCULATEEUCLIDEANDIST Summary of this function goes here
-%   Detailed explanation goes here
+%CALCULATEEUCLIDEANDIST Calculates Euclidean distance from a set of points
+% to a coordinate
+%   DISTANCE = CALCULATEEUCLIDEANDIST(COORDS, COORD) returns the Euclidean
+%   distance from a set of coordinates COORDS to a given coordinate COORD.
+%   COORDS is an Nx2-vector, while COORD is 1x2.
+
 deltaX = bsxfun(@minus, coords(:, 1), coord(1));
 deltaY = bsxfun(@minus, coords(:, 2), coord(2));
 
