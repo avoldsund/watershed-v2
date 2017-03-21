b7bbbe2272dc0e95ad959d5f77d36710fa670029
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-03-21 15:07:11 +0100

    Reimplement method to scale fluxes

--- mrst-code/+util/calculateFlux.m
+++ mrst-code/+util/calculateFlux.m
@@ -1,36 +1,46 @@
-function dotProduct = calculateFlux(CG, faceNormals, faceFlowDirections)
-%SCALEDOTPRODUCT Scale the flux based on slope or other parameters
-%   DOTPRODUCT = SCALEDOTPRODUCT(CG, DOTPRODUCT) scales the flux based on
-%   different parameters such as slope, and returns the new DOTPRODUCT.
+function flux = calculateFlux(CG, faceNormals, faceFlowDirections)
+%CALCULATEFLUX Something
+%   
+    flux = sum(faceNormals .* faceFlowDirections, 2);
     
-    dotProduct = sum(faceNormals .* faceFlowDirections, 2);
-
-    %N = size(CG.cells.faces, 1);
-    %faceIndices = CG.cells.faces(:, 1);
-    %nbrsOfFaces = CG.faces.neighbors(faceIndices, :);
-    %bothNbrsInInterior = all(nbrsOfFaces ~= 0, 2);
-    %validIndices = nbrsOfFaces(bothNbrsInInterior, :);
-    %deltaZ = zeros(N, 1);
+    N = size(CG.cells.faces, 1);
+    deltaZ = zeros(N, 1);
+    
     for i = 1:CG.cells.num
-        faces = util.getCellFaces(CG, i)
-        startIx = CG.cells.facePos(i);
-        endIx = CG.cells.facePos(i + 1) - 1;
-        dotProduct(startIx:endIx)
+        indices = CG.cells.facePos(i) : CG.cells.facePos(i + 1) - 1;
+        [faceFlipped, nrmls, sign] = util.flipNormalsOutwards(CG, i);
+        faceFluxes = sign .* flux(indices);
+        
+        nbrs = CG.faces.neighbors(faceFlipped, :);
+        nbrs(sign == -1, :) = fliplr(nbrs(sign == -1, :));
+        invalidIndices = nbrs(:, 1) == 0 | nbrs(:, 2) == 0;
+        validIndices = nbrs(:, 1) ~= 0 & nbrs(:, 2) ~= 0;
+
+        ixValid = indices(validIndices);
+        ixInvalid = indices(invalidIndices);
+        validNbrs = nbrs(validIndices, :);
+        validD = faceFluxes(validIndices, :);
+        outFlow = validD > 0;
+        inFlow = validD < 0;
+        
+        deltaOutFlow = CG.cells.z(validNbrs(outFlow, 1)) - CG.cells.z(validNbrs(outFlow, 2));
+        deltaInFlow = CG.cells.z(validNbrs(inFlow, 2)) - CG.cells.z(validNbrs(inFlow, 1));
+        
+        ixOut = ixValid(outFlow);
+        posOut = deltaOutFlow > 0;
+        negOut = deltaOutFlow <= 0;
+        
+        deltaZ(ixOut(negOut)) = 0.1;
+        deltaZ(ixOut(posOut)) = deltaOutFlow(posOut);
+        
+        ixIn = ixValid(inFlow);
+        posIn = deltaInFlow > 0;
+        negIn = deltaInFlow <= 0;
+        deltaZ(ixIn(negIn)) = 0.1;
+        
+        deltaZ(ixIn(posIn)) = deltaInFlow(posIn);
     end
     
-    % Absolute value of slope 
-    %deltaZ(bothNbrsInInterior) = abs(CG.cells.z(validIndices(:, 1)) - CG.cells.z(validIndices(:, 2)));
-    
-    % Negative slopes set to constant
-    %deltaZ(bothNbrsInInterior) = CG.cells.z(validIndices(:, 1)) - CG.cells.z(validIndices(:, 2));
-    %uphillSlope = find(deltaZ < 0);
-    %deltaZ(uphillSlope) = 0.2;
-    
-    % Get indices where deltaZ is zero
-    %indicesOfZeroDeltaZ = all(deltaZ == 0, 2);
-    %deltaZ(indicesOfZeroDeltaZ) = 0.1;
-    %dotProduct = dotProduct .* deltaZ;
-    
-    % Divide by distance
-    dotProduct = dotProduct * 0.1;
+    flux = flux .* deltaZ;
+    flux = flux * 0.1;
 end
