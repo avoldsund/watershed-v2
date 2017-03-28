1424909260fb960708fc17795efae1971a7e47c4
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-03-28 19:38:45 +0200

    Many minor changes to script

--- mrst-code/calculateTofUsingLandscape.m
+++ mrst-code/calculateTofUsingLandscape.m
@@ -3,6 +3,12 @@
 % From pre-processing: if a trap is spilling over into a cell at equal
 % height, add that cell to the trap
 
+% Colors
+scale = 255;
+blueBrewer = [140, 160, 203] ./ scale;
+greenBrewer = [102, 194, 165] ./ scale;
+orangeBrewer = [252, 141, 98] ./scale;
+
 % Load necessary data and compute geometry
 load('watershed.mat');
 load('heights.mat');
@@ -14,33 +20,35 @@ load('steepest.mat')
 totalCells = nRows * nCols;
 
 % Pre-process input data, create coarse grid and set heights
+sideLength = 10;
 [heights, fd, ws, spillPairsIndices] = util.preProcessData(heights, flowDirections, watershed, spillPairs);
-CG = util.createCoarseGrid(ws, heights, traps, nrOfTraps, spillPairs);
+CG = util.createCoarseGrid(ws, heights, traps, nrOfTraps, spillPairs, sideLength);
 CG.cells.z = util.setHeightsCoarseGrid(CG, heights, trapHeights, nrOfTraps);
 
 % Plot landscape with traps
 figure();
 newplot
 colorIndices = zeros(CG.cells.num, 1);
-colorIndices(CG.cells.num - nrOfTraps + 1:end) = 1:nrOfTraps;
-plotCellData(CG,colorIndices,'EdgeColor','w','EdgeAlpha',.2);
-
+%timecolorIndices(CG.cells.num - nrOfTraps + 1:end) = 1:nrOfTraps;
+%plotCellData(CG,colorIndices,'EdgeColor',greenBrewer,'EdgeAlpha',3);
+plotGrid(CG)
 %% Show cell/block indices
 % In its basic form, the structure only represents topological information
 % that specifies the relationship between blocks and block interfaces, etc.
 % The structure also contains information of the underlying fine gr id. Let
 % us start by plotting cell/block indices
-tg = text(CG.parent.cells.centroids(:,1), CG.parent.cells.centroids(:,2), ...
-   num2str((1:CG.parent.cells.num)'),'FontSize',8, 'HorizontalAlignment','center');
+
+%tg = text(CG.parent.cells.centroids(:,1), CG.parent.cells.centroids(:,2), ...
+%   num2str((1:CG.parent.cells.num)'),'FontSize',8, 'HorizontalAlignment','center');
 tcg = text(CG.cells.centroids(:,1), CG.cells.centroids(:,2), ...
    num2str((1:CG.cells.num)'),'FontSize',16, 'HorizontalAlignment','center');
-set(tcg,'BackgroundColor','w','EdgeColor','none');
+set(tcg,'BackgroundColor','w','EdgeColor','None');
 colormap(.5*jet+.5*ones(size(jet)));
 
 %% Show face indices of fine/coarse grids
-delete([tg; tcg]);
-tg = text(CG.parent.faces.centroids(:,1), CG.parent.faces.centroids(:,2), ...
-   num2str((1:CG.parent.faces.num)'),'FontSize',7, 'HorizontalAlignment','center');
+%delete([tg; tcg]);
+%tg = text(CG.parent.faces.centroids(:,1), CG.parent.faces.centroids(:,2), ...
+%   num2str((1:CG.parent.faces.num)'),'FontSize',7, 'HorizontalAlignment','center');
 tcg = text(CG.faces.centroids(:,1), CG.faces.centroids(:,2), ...
    num2str((1:CG.faces.num)'),'FontSize',12, 'HorizontalAlignment','center');
 set(tcg,'BackgroundColor','w','EdgeColor','none');
@@ -48,16 +56,17 @@ set(tcg,'BackgroundColor','w','EdgeColor','none');
 %% Perform time-of-flight
 
 % Add flux field, state, rock and source
-[src, trapNr] = util.getSource(CG, outlet, traps, nCols);
+srcStrength = 1;
+[src, trapNr] = util.getSource(CG, outlet, traps, nCols, srcStrength);
 CG.cells.fd = util.getFlowDirections(CG, fd, nrOfTraps, spillPairsIndices);
 [flux, faceFlowDirections] = util.setFlux(CG, nrOfTraps, trapNr);
 state = struct('flux', flux);
-rock = util.setPorosity(CG, nrOfTraps, 0.005);
+rock = util.setPorosity(CG, nrOfTraps, 1);
 
 % Calculate time-of-flight
-max_time = 200000;
+maxTime = 3000;
 tof = computeTimeOfFlight(state, CG, rock, 'src', src, ...
-   'maxTOF', max_time, 'reverse', true);
+   'maxTOF', maxTime, 'reverse', true);
 
 % Plot results
 figure()
@@ -68,11 +77,10 @@ colormap(jet)
 %caxis([0, 120000/timeScale])
 
 %% Make uniform hydrograph
-
 timeScale = 60;
 amount = 1;
 duration = 5;
-tof = floor(tof ./ timeScale);
+tof = ceil(tof ./ timeScale);
 
 hydrograph = util.hydrographUniform(CG, tof, amount, duration);
 plot(hydrograph)
@@ -83,24 +91,3 @@ else
     xlabel('Time (hours)')
 end
 ylabel('Flow m^3/s')
-
-
-%% Make disc hydrograph
-%timeScale = 60;
-%tof = ceil(tof ./ timeScale);
-
-% Direction and speed of disc precipitation
-d = [1, 1];
-v = 100;
-
-% Disc properties
-c0 = [10, 10];
-r = 500;
-amount = 1;
-disc = struct('radius', r, 'center', c0, 'amount', amount,...
-    'direction', d, 'speed', v);
-
-hydrograph = util.hydrographMovingDisc(CG, tof, disc);
-
-
-%% 
