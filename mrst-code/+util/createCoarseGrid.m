6467fe19f8c812cc3800e32ab908fa91b10d9a78
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-03-28 19:31:28 +0200

    Add stepSize parameter to change area of cells

--- mrst-code/+util/createCoarseGrid.m
+++ mrst-code/+util/createCoarseGrid.m
@@ -1,6 +1,6 @@
-function CG = createCoarseGrid(watershed, heights, traps, nrOfTraps, spillPairs)
+function CG = createCoarseGrid(watershed, heights, traps, nrOfTraps, spillPairs, stepSize)
 %CREATECOARSEGRID Creates a coarse grid based on watershed and traps. 
-%   CG = CREATECOARSEGRID(WATERSHED, HEIGHTS, TRAPS, NROFTRAPS, SPILLPAIRS)
+%   CG = CREATECOARSEGRID(WATERSHED, HEIGHTS, TRAPS, NROFTRAPS, SPILLPAIRS, STEPSIZE)
 %   first creates a cartGrid the same size as the HEIGHTS-landscape. Each
 %   cell is stepSize x stepSize large. Some cells are removed, so only the
 %   WATERSHED cells remain. Afterwards TRAPS are combined in a process using
@@ -10,7 +10,7 @@ function CG = createCoarseGrid(watershed, heights, traps, nrOfTraps, spillPairs)
 [nRows, nCols] = size(heights);
 
 % Create entire nRows x nCols-grid and remove cells outside watershed
-stepSize = 10;
+%stepSize = 10;
 G = cartGrid([nCols, nRows], [nCols * stepSize, nRows * stepSize]);
 G = computeGeometry(G);
 rmCells = setdiff(1 : nCols * nRows, watershed);
