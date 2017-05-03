9d3b95038b9ec65fd6cdebf03a0300e9a3c05af7
Anders Opskar Voldsund <andersvoldsund@gmail.com>
2017-05-03 11:51:33 +0200

    Make changes to increase speed

--- mrst-code/+util/flipNormalsOutwards.m
+++ mrst-code/+util/flipNormalsOutwards.m
@@ -8,7 +8,9 @@ startIx = CG.cells.facePos(cellIx);
 endIx = CG.cells.facePos(cellIx + 1) - 1;
 faces = CG.cells.faces(startIx:endIx, 1);
 faceNormals = CG.faces.normals(faces, :);
-sign = 1 - 2 * (CG.faces.neighbors(faces, 1) ~= rldecode(cellIx, size(faces, 1)));
+
+nbrNotCellIx = CG.faces.neighbors(faces, :) ~= cellIx;
+sign = 1 - 2 * nbrNotCellIx(:, 1);
 faceNormals(sign == -1, :) = -faceNormals(sign == -1, :);
 
 end
